343d6695f58476b7029c9033f31fa4d88bf1f442
Linus Schärmann <linus.schaermann@managrsphere.com>
2024-06-17 15:07:59 +0200

    Added Links to Navbar and Footer

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -11,36 +11,22 @@
                 </div>
                 <ul tabindex="0"
                     class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <a>Parent</a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><a>Über uns</a></li>
+                    <li><a>Chargia</a></li>
+                    <li><a>Semesterprogramm</a></li>
                 </ul>
             </div>
-            <a class="btn btn-ghost text-xl">daisyUI</a>
+            <a class="btn btn-ghost text-xl">Oeno Danubia</a>
         </div>
         <div class="navbar-center hidden lg:flex">
             <ul class="menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li>
-                    <details>
-                        <summary>Parent</summary>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </details>
-                </li>
-                <li><a>Item 3</a></li>
+                <li><a>Über uns</a></li>
+                <li><a>Chargia</a></li>
+                <li><a>Semesterprogramm</a></li>
             </ul>
         </div>
         <div class="navbar-end">
-            <a class="btn">Button</a>
+            <a class="btn">Kontakt</a>
         </div>
     </div>
 </template>
